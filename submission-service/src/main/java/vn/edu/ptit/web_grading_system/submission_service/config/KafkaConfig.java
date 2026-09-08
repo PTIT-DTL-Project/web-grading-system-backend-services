87764ec127734ddb3ccfd2c3134bfb20ab785d29
Duong Vu Cong Tuan <vucongtuanduong2409@gmail.com>
2026-09-08 22:58:31 +0700

    fix local cannot init kafka

---
+++ submission-service/src/main/java/vn/edu/ptit/web_grading_system/submission_service/config/KafkaConfig.java
@@ -0,0 +1,62 @@
+package vn.edu.ptit.web_grading_system.submission_service.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import vn.edu.ptit.web_grading_system.submission_service.event.WgsEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.properties.security.protocol:SASL_SSL}")
+    private String securityProtocol;
+
+    @Value("${spring.kafka.properties.sasl.mechanism:SCRAM-SHA-256}")
+    private String saslMechanism;
+
+    @Value("${spring.kafka.properties.sasl.jaas.config:}")
+    private String saslJaasConfig;
+
+    @Value("${spring.kafka.properties.ssl.endpoint.identification.algorithm:https}")
+    private String sslEndpointIdentificationAlgorithm;
+
+    @Value("${spring.kafka.properties.ssl.truststore.type:PEM}")
+    private String sslTruststoreType;
+
+    @Value("${spring.kafka.properties.ssl.truststore.location:docker/kafka-ca.pem}")
+    private String sslTruststoreLocation;
+
+    @Bean
+    public ProducerFactory<String, WgsEvent<?>> wgsProducerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put("security.protocol", securityProtocol);
+        props.put("sasl.mechanism", saslMechanism);
+        if (saslJaasConfig != null && !saslJaasConfig.isBlank()) {
+            props.put("sasl.jaas.config", saslJaasConfig);
+        }
+        props.put("ssl.endpoint.identification.algorithm", sslEndpointIdentificationAlgorithm);
+        props.put("ssl.truststore.type", sslTruststoreType);
+        props.put("ssl.truststore.location", sslTruststoreLocation);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props, new StringSerializer(), new JsonSerializer<>());
+    }
+
+    @Bean
+    public KafkaTemplate<String, WgsEvent<?>> wgsKafkaTemplate() {
+        return new KafkaTemplate<>(wgsProducerFactory());
+    }
+}
