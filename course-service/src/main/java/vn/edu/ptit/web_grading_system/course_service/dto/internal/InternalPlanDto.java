f3beeba51b9c5b52f13280e752e48a879fcf14c5
Duong Vu Cong Tuan <vucongtuanduong2409@gmail.com>
2026-09-16 22:55:56 +0700

    feat: update course service for grading result

--- course-service/src/main/java/vn/edu/ptit/web_grading_system/course_service/dto/internal/InternalPlanDto.java
+++ course-service/src/main/java/vn/edu/ptit/web_grading_system/course_service/dto/internal/InternalPlanDto.java
@@ -2,13 +2,19 @@ package vn.edu.ptit.web_grading_system.course_service.dto.internal;
 
 import java.util.List;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Builder
-public record InternalPlanDto(
-        UUID id,
-        String name,
-        Integer sequenceOrder,
-        Integer weight,
-        List<InternalStepDto> steps) {
+@NoArgsConstructor
+@AllArgsConstructor
+public class InternalPlanDto {
+    private UUID id;
+    private String name;
+    private Integer sequenceOrder;
+    private Integer weight;
+    private List<InternalStepDto> steps;
 }
--- course-service/src/main/java/vn/edu/ptit/web_grading_system/course_service/dto/internal/InternalStepDto.java
+++ course-service/src/main/java/vn/edu/ptit/web_grading_system/course_service/dto/internal/InternalStepDto.java
@@ -1,17 +1,23 @@
 package vn.edu.ptit.web_grading_system.course_service.dto.internal;
 
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Builder
-public record InternalStepDto(
-        UUID id,
-        Integer stepOrder,
-        String name,
-        String stepType,
-        String config,
-        String expectedResult,
-        Integer weight,
-        Integer timeoutMs,
-        Boolean required) {
+@NoArgsConstructor
+@AllArgsConstructor
+public class InternalStepDto {
+    private UUID id;
+    private Integer stepOrder;
+    private String name;
+    private String stepType;
+    private String config;
+    private String expectedResult;
+    private Integer weight;
+    private Integer timeoutMs;
+    private Boolean required;
 }
